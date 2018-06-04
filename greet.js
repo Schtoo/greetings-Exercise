b09da04f3aad0fc49e43a7b19d7505d532d2e43c
Schtoo <mgqobhoz@gmail.com>
2018-06-04 15:18:32 +0200

    added greetings html & css

---
+++ greet.js
@@ -0,0 +1,14 @@
+var Greetings = 0;
+var counter = 0;
+var button = document.querySelector(".btn");
+var reset = document.querySelector(".reset");
+var textArea = document.querySelector(".text");
+var greetCount = {};
+
+function greeted() {
+  if (textArea === undefined) {}
+  return "error, please insert a name"
+}
+
+button.addEventLisener("click", button);
+reset.addEventLisener("click", reset);
